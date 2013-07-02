e730e8b0afcc928b0cf48e28cb6268e822d797eb
Simon Johansson <simon@editd.com>
2013-07-02 12:23:15 +0100

    Install git if including git_ssh_wrapper in metadata and git isnt there already.

--- metadata.rb
+++ metadata.rb
@@ -6,3 +6,4 @@ description      "Installs/Configures git_ssh_wrapper"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.1"
 
+depends "git", ">= 2.5.2"
